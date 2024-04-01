913bda7ddcc9eb25ab57d26717ffc618af676842
Harriet Fisher <harrietfisher@Harriets-MacBook-Pro.local>
2024-04-01 14:39:12 -0500

    changes to player script

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 public float speed = 12f;
+public float gravity = -9.81f;
+public Transform groundCheck;
+public float groundDistance = 0.4f;
+public LayerMask groundMask;
+
+Vector3 velocity;
 public CharacterController controller;
     // Start is called before the first frame update
     void Start()
@@ -15,10 +21,20 @@ public CharacterController controller;
     // Update is called once per frame
     void Update()
     {
+        bool isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis ("Horizontal");
         float z = Input.GetAxis ("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
